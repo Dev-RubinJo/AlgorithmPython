5ec824a615e7397128e190099bafbdaaf682d7aa
Rubin.Jo <dbqlsdl1234@naver.com>
2020-09-04 23:01:27 +0900

    1520 해결 18247 해결 연구소 문제 우선 해결 다만 다시 꼭 풀어봐야할 문제 특정 거리의 도시 찾기 문제 해결 다만 꼭 다시 풀어봐야할 문제 체육복 코드 수정 1707 미해결

---
+++ ProblemSolving/BackJoon/1707.py
@@ -0,0 +1,4 @@
+k = int(input())
+
+for _ in range(k):
+    v, e = map(int, input().split())
--- ProblemSolving/프로그래머스/Greedy/체육복.py
+++ ProblemSolving/프로그래머스/Greedy/체육복.py
@@ -1,31 +1,3 @@
-def solution_worng(n, lost, reserve):
-    answer = 0
-    answer2 = 0
-    take = []
-    set_reserve = set(reserve) - set(lost)
-    set_lost = set(lost) - set(reserve)
-    for i in range(n):
-        if i + 1 in set_reserve:
-            take.append(1)
-            answer += 1
-            answer2 += 1
-        elif i + 1 in set_lost:
-            take.append(0)
-        else:
-            take.append(0)
-            answer += 1
-            answer2 += 1
-
-    for i in range(n):
-        if i + 1 in set_lost:
-            if i - 1 >= 0 and take[i - 1] == 1:
-                answer += 1
-    for i in range(n - 1, -1, -1):
-        if i in set_lost:
-            if take[i] == 1:
-                answer2 += 1
-
-    return max(answer, answer2)
 
 def solution(n, lost, reserve):
     set_reserve = set(reserve) - set(lost)
