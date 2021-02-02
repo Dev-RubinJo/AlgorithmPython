5f673a2f933620ea73510e5e64fa84831616c063
Rubin.Jo <dbqlsdl1234@naver.com>
2021-02-02 23:19:28 +0900

    백준 14620 실패 사실 구현을 못하겠어서 다른 사람의 풀이를 보고 커스텀해서 작성했는데 계속 answer에서 none 값이 들어간다

---
+++ ProblemSolving/BackJoon/BFS and DFS/14620-1.py
@@ -0,0 +1,39 @@
+def check(x, y, n, dx, dy, visited):
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 > nx or nx > n - 1 or 0 > ny or ny > n - 1 or visited[nx][ny]:
+            return False
+    return True
+
+def calculate(x, y, dx, dy, ground):
+    result = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        result += ground[nx][ny]
+    return result
+
+def dfs(x, n, cost, count, answer, dx, dy, ground, visited):
+    if count == 3:
+        answer = min(answer, cost)
+        return answer
+    for i in range(x, n):
+        for j in range(1, n):
+            if check(i, j, n, dx, dy, visited):
+                visited[i][j] = True
+                for k in range(4):
+                    visited[i + dx[k]][j + dy[k]] = True
+                dfs(i, n, cost + calculate(i, j, dx, dy, ground), count + 1, answer, dx, dy, ground, visited)
+                visited[i][j] = False
+                for k in range(4):
+                    visited[i + dx[k]][j + dy[k]] = False
+
+def solution14620():
+    n = int(input())
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    ground = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[False] * n for _ in range(n)]
+    answer = 99999999999
+    answer = dfs(1, n, 0, 0, answer, dx, dy, ground, visited)
+    print(answer)
+solution14620()
