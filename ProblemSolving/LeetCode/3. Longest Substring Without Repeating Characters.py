6976e3c4c7ea87c7983a233397e4f24bb72e3134
Rubin.Jo <dbqlsdl1234@naver.com>
2021-03-11 19:57:41 +0900

    릿코드 LongestSubstringWithoutRepeatingCharacters

---
+++ ProblemSolving/LeetCode/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        queue = deque([])
+        window = set()
+        result = 0
+
+        for c in s:
+            if c in window:
+                while queue:
+                    prev = queue.popleft()
+                    window.remove(prev)
+                    if prev == c:
+                        break
+
+            queue.append(c)
+            window.add(c)
+            result = max(result, len(window))
+
+        return result
+
+print(Solution().lengthOfLongestSubstring(" "))
